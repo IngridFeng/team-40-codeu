29b8c9c26ff36bef64431a488ea4e99683f86d46
Michelle <michlee1337@gmail.com>
2019-06-26 22:46:47 +0900

    upon creating a chat, user is redirected to chat page

--- src/main/java/com/google/codeu/servlets/ChatServlet.java
+++ src/main/java/com/google/codeu/servlets/ChatServlet.java
@@ -97,56 +97,12 @@ public class ChatServlet extends HttpServlet {
       System.out.println(user.getChats());
       System.out.println(selectedUser.getChats());
 
+      // redirect to chat package
+      response.sendRedirect("/chat.html?chat=" + chat.getId());
+      return;
+
+
+
     }
 
 }
-
-
-
-  /**
-   * upon posting, log a new chat
-   * currently, only direct messages. Groups in the future.
-   */
-
-    /**
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // get the users involved
-    List<String> users = request.getParameter('users');
-
-    // get the chat name
-    String chatName = request.getParameter('chatName');
-    String chatDescription = request.getParameter('chatDescription');
-
-    // create new chat
-    chat = new Chat(chatName, chatDescription);
-
-    // store the new chat instance for each user
-    while (users.hasNext()) {
-      try {
-        System.oit.println(iterator.next());
-      } catch (Exception e) {
-        System.err.println("Error reading message.");
-        System.err.println(entity.toString());
-        e.printStackTrace();
-      }
-    }
-  }
-
-  **/
-
-  // send users to the new chat
-
-  /**
-    * upon getting, read all chats and return a JSON?
-     check with userlist servlet
-  */
-
-
-    /**
-    throws IOException {
-
-    }
-
-    response.getOutputStream().println();
-    **/
--- src/main/webapp/js/chat-page-loader.js
+++ src/main/webapp/js/chat-page-loader.js
@@ -1,3 +1,30 @@
+// Get ?chat=XYZ parameter value
+const urlParams = new URLSearchParams(window.location.search);
+const parameterChat = urlParams.get('chat');
+// if chat parameter not in url, redirect to home
+if (!parameterChat) {
+  window.location.replace('/');
+}
+
+// get chat details
+
+
+// set the chat title to the saved chat name
+function setChatName() {
+  if (viewingSelf == true){
+    document.getElementById('page-title').innerText = 'Welcome home! ' + nickName.replace(/(\n|\r|\r\n)/g, '') + ' ^_^';
+  }
+  else{
+    if (nickName == ''){
+      document.getElementById('page-title').innerText = 'Hello! My owner hasn\'t set my name yet! Please remind him/her to give me a name ^_^';
+    }
+    else{
+      document.getElementById('page-title').innerText = 'Helloooo! This is ' + nickName.replace(/(\n|\r|\r\n)/g, '') + ". Welcome to my page ^_^";
+    }
+  }
+
+
+
 // send get request to chat endpoint
 
 // pull up requested chat id
