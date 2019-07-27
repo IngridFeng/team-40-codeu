0ddf2cf823ae84048bdc8c106d4620a36a9f06f9
Tenuun Olonbaatar <42817849+tolonbaa@users.noreply.github.com>
2019-07-27 14:18:35 +0800

    Update feed-loader.js

--- src/main/webapp/js/feed-loader.js
+++ src/main/webapp/js/feed-loader.js
@@ -34,6 +34,7 @@
     }).then((nickName) => {
       const userName = nickName || message.user;
       const userLinkText = document.createTextNode(userName);
+      userLinkText.style.color = "blue";
       userLink.appendChild(userLinkText);
     });
     //const userLinkText = document.createTextNode(userName);
